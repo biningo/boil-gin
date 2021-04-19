f179477d040ef449cbe0dcafe2d2755e04b9c3bf
biningo <biningo.cn@gmail.com>
2021-04-19 23:11:00 +0800

    feat:usr follow table

--- boil.sql
+++ boil.sql
@@ -8,25 +8,23 @@ CREATE TABLE boil_user
     bio       VARCHAR(100)                   NOT NULL
 );
 
-# CREATE TABLE boil_user_msg
-# (
-#     id          INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-#     user_id     INT                            NOT NULL,
-#     create_time DATETIME                       NOT NULL,
-#     content     varchar(50)                    NOT NULL,
-#     bio_id      INT                            NOT NULL,
-#     FOREIGN KEY (user_id) REFERENCES boil_user (id) ON DELETE CASCADE,
-#     FOREIGN KEY (bio_id) REFERENCES boil_boil (id) ON DELETE CASCADE
-# );
+CREATE TABLE boil_user_follow_user
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    follower_id INT                            NOT NULL,
+    user_id     INT                            NOT NULL,
+    FOREIGN KEY (follower_id) REFERENCES boil_user (id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES boil_user (id) ON DELETE CASCADE
+);
 
 
 CREATE TABLE boil_boil
 (
-    id            INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    tag_id        INT                            NOT NULL,
-    user_id       INT                            NOT NULL,
-    create_time   DATETIME                       NOT NULL,
-    content       VARCHAR(300)                   NOT NULL,
+    id          INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    tag_id      INT                            NOT NULL,
+    user_id     INT                            NOT NULL,
+    create_time DATETIME                       NOT NULL,
+    content     VARCHAR(300)                   NOT NULL,
     FOREIGN KEY (tag_id) REFERENCES boil_tag (id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES boil_user (id) ON DELETE CASCADE
 );
@@ -43,15 +41,14 @@ CREATE TABLE boil_comment
     FOREIGN KEY (user_id) REFERENCES boil_user (id) ON DELETE CASCADE
 );
 
-# CREATE TABLE boil_user_like_boil
-# (
-#     id          INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-#     user_id     INT                            NOT NULL,
-#     boil_id     INT                            NOT NULL,
-#     create_time DATETIME                       NOT NULL,
-#     FOREIGN KEY (user_id) REFERENCES boil_user (id) ON DELETE CASCADE,
-#     FOREIGN KEY (boil_id) REFERENCES boil_boil (id) ON DELETE CASCADE
-# );
+CREATE TABLE boil_user_like_boil
+(
+    id      INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    user_id INT                            NOT NULL,
+    boil_id INT                            NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES boil_user (id) ON DELETE CASCADE,
+    FOREIGN KEY (boil_id) REFERENCES boil_boil (id) ON DELETE CASCADE
+);
 
 CREATE TABLE boil_tag
 (
